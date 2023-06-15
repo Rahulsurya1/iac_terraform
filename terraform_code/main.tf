8db25252cddfcb741913cc6a62d5de0245a518ab
Rahulsurya1 <121539105+Rahulsurya1@users.noreply.github.com>
2023-06-15 15:17:11 +0530

    Update main.tf

--- terraform_code/main.tf
+++ terraform_code/main.tf
@@ -41,18 +41,6 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-  security_rule {
-    name                       = "added_1"
-    priority                   = 1010
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "8989"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
 }
 # Create network interface
 resource "azurerm_network_interface" "my_terraform_nic" {
